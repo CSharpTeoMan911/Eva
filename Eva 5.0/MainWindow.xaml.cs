ddf687c4d8fa7c609c4e18e9e25069be9c765f9d
CSharpTeoMan911 <teodormihail07@gmail.com>
2022-10-26 14:00:04 +0100

    * OPT

--- Eva 5.0/MainWindow.xaml.cs
+++ Eva 5.0/MainWindow.xaml.cs
@@ -838,7 +838,7 @@ namespace Eva_5._0
                                                             if (Online_Speech_Recogniser_Taking_Input == "false")
                                                             {
 
-                                                                if (Online_Speech_Recogniser_Not_Taking_Input_While_Activated_TimeOut > 100)
+                                                                if (Online_Speech_Recogniser_Not_Taking_Input_While_Activated_TimeOut > 180)
                                                                 {
                                                                     Task.Run(async () =>
                                                                     {
