8fccd3485ca34ddae8795f90828c18a8c5b78e79
CSharpTeoMan911 <teodormihail07@gmail.com>
2023-01-21 18:38:43 +0000

    * WAKE WORD ENGINE CALIBRATION

--- Eva 5.0/main.py
+++ Eva 5.0/main.py
@@ -4,7 +4,7 @@ import socket
 import sys
 
 
-speech = LiveSpeech(lm=False, keyphrase='eva', kws_threshold=1e-12)
+speech = LiveSpeech(lm=False, keyphrase='eva', kws_threshold=1e-9)
 
 
 def wake_word_operation_application_socket():
