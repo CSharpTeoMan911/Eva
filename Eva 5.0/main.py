dc23b23d88d7e06ccd5569f3db33747e99d9c72e
CSharpTeoMan911 <teodormihail07@gmail.com>
2023-02-04 14:08:43 +0000

    * OPTIMISED WAKE WORD ENGINE

--- Eva 5.0/main.py
+++ Eva 5.0/main.py
@@ -7,7 +7,7 @@ import time
 import sys
 
 
-speech = LiveSpeech(lm=False, keyphrase='eva wake up', kws_threshold=1e-32)
+speech = LiveSpeech(lm=False, keyphrase='eva wake up', kws_threshold=1e-48)
 
 
 def wake_word_operation_application_socket():
@@ -35,7 +35,7 @@ def Wake_Word_Initiation():
     # WAKE WORD ENGINE OPERATION #
     ##############################
     global speech
-    speech = LiveSpeech(lm=False, keyphrase='eva wake up', kws_threshold=1e-32)
+    speech = LiveSpeech(lm=False, keyphrase='eva wake up', kws_threshold=1e-48)
     try:
         try:
             for phrase in speech:
