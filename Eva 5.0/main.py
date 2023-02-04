582186c8a296d661dca913b8046ca3e2070b77b7
CSharpTeoMan911 <teodormihail07@gmail.com>
2023-02-04 22:14:27 +0000

    * CHANGED THE WAKE WORD TO "Wake Eva"

--- Eva 5.0/main.py
+++ Eva 5.0/main.py
@@ -7,7 +7,7 @@ import time
 import sys
 
 
-speech = LiveSpeech(lm=False, keyphrase=' eva wake up ', kws_threshold=1e-50)
+speech = LiveSpeech(lm=False, keyphrase=' wake eva ', kws_threshold=1e-30)
 
 
 def wake_word_operation_application_socket():
@@ -35,7 +35,7 @@ def Wake_Word_Initiation():
     # WAKE WORD ENGINE OPERATION #
     ##############################
     global speech
-    speech = LiveSpeech(lm=False, keyphrase=' eva wake up ', kws_threshold=1e-50)
+    speech = LiveSpeech(lm=False, keyphrase=' wake eva ', kws_threshold=1e-30)
     try:
         try:
             for phrase in speech:
