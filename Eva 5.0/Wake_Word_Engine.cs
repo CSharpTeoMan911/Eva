40dbc838a026930aa4a7df1f19c8f8f28794bad0
Teodor Mihail <teodormihail07@gmail.com>
2024-03-21 01:16:41 +0000

    * FIXED CALIBRATION ISSUE

--- Eva 5.0/Wake_Word_Engine.cs
+++ Eva 5.0/Wake_Word_Engine.cs
@@ -57,7 +57,7 @@ namespace Eva_5._0
         private static string wake_word = "listen";
         private static bool Wake_Word_Started = false;
 
-        private static int wake_word_engine_reset_time = 1000;
+        private static int wake_word_engine_reset_time = 1500;
  
 
         public static void Start_The_Wake_Word_Engine()
