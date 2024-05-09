3408e43d311744663ae0c2c97a406bb300fed8fa
Teodor Mihail <teodormihail07@gmail.com>
2024-05-09 17:35:47 +0100

    * ADDED WINDOWS DEFENDER SCAN EXCLUSION

---
+++ Eva 5.0/IntialSetup.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Eva_5._0
+{
+    /// <summary>
+    /// Interaction logic for IntialSetup.xaml
+    /// </summary>
+    public partial class IntialSetup : Window
+    {
+        public IntialSetup()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Eva 5.0/WD_Enable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Eva_5._0
+{
+    internal class WD_Enable
+    {
+        public static async Task<bool> CheckIfEnabled()
+        {
+            
+
+            StringBuilder args_builder = new StringBuilder("/k \"\"");
+            args_builder.Append(Environment.CurrentDirectory);
+            args_builder.Append("\\win-x86\\WPF_WD_ALLOW.exe\" \"");
+            args_builder.Append(Environment.CurrentDirectory);
+            args_builder.Append("\"\"");
+
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = args_builder.ToString();
+            process.StartInfo.UseShellExecute = true;
+            process.Start();
+
+
+            NamedPipeServerStream pipeServer = new NamedPipeServerStream("WD_PIPE", PipeDirection.In);
+            pipeServer.WaitForConnection();
+
+            byte[] buffer = new byte[Encoding.UTF8.GetBytes("T").Length];
+            await pipeServer.ReadAsync(buffer, 0, buffer.Length);
+
+            string result = Encoding.UTF8.GetString(buffer);
+
+            if (result == "T")
+            {
+                await Settings.Set_Initial_Setup(true);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
