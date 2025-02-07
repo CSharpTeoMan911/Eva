0155c57527b9723e4965729557a9d2c59ee9effb
Teodor Mihail <teodormihail07@gmail.com>
2025-02-07 03:26:44 +0000

    * MINOR CHANGE

--- Eva 5.0/Online_Speech_Recognition.cs
+++ Eva 5.0/Online_Speech_Recognition.cs
@@ -133,6 +133,9 @@ namespace Eva_5._0
 
                         await OS_Online_Speech_Recognition_Interface_Shutdown_Or_Refresh(Online_Speech_Recognition_Interface_Operation.Online_Speech_Recognition_Interface_Clear_Cache);
                         await OnlineSpeechRecognition.ContinuousRecognitionSession.StartAsync(Windows.Media.SpeechRecognition.SpeechContinuousRecognitionMode.PauseOnRecognition);
+
+                        await OnlineSpeechRecognition.ContinuousRecognitionSession.PauseAsync();
+                        OnlineSpeechRecognition.ContinuousRecognitionSession.Resume();
                         break;
 
                     case false:
